856c884a58f38db78b5467945fe63a8be2614383
Roberto v/d Meer <robertovdmeer@gmail.com>
2019-06-26 22:14:00 +0200

    Added functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class LandingPageTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_homepage_loads(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('Meet My Poops', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
