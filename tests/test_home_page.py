37835e84d9424208211771e95b0522553f7cc4d3
Roberto v/d Meer <robertovdmeer@gmail.com>
2019-06-28 09:50:54 +0200

    Basic homepage

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,7 +1,21 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+from meetmypeeps.views import home_page
 
 
-class HomepageTests(TestCase):
+class HomepageTest(TestCase):
 
-    def test_http_response(self):
-        self.assertEquals(True, False)
+    def test_root_resolves_to_homepage(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Meet My Peeps', html)
+        self.assertTrue(html.endswith('</html>'))
+
